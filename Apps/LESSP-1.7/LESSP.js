6284f537b74a87b9c68639c31499b416561bd52e
Tiggert <nicliff@gmail.com>
2012-08-31 16:50:00 +1000

    Bunch of changes to incorporate JS calls

--- Apps/LESSP-1.7/LESSP.js
+++ Apps/LESSP-1.7/LESSP.js
@@ -17,12 +17,52 @@
 	);
 });
 
+//replace this once we get the database in place
+function getCustomerName(){
+	return "Nick";
+}
+
+function getName2(){
+	return "Nick";
+}
+
+//replace this once we get the database in place
+function getRegularCustomerAccountsAndValue(){
+	var regValues = new Array(4);
+	regValues[0] = "Account 1";
+	regValues[1] = "Account 2";
+	regValues[2] = 20.00;
+	regValues[3] = "20.00"
+	
+	return regValues;
+}
+
+//replace this once we get the database in place
+//Use IDs instead of weekly etc in db (then we can do monthly/fortnightly etc)
+function howOften(){
+	var when = new Array(7);
+	when[0] = "weekly";
+	when[1] = 1; //code for when[0] (hardcoded to 'weekly' currently)
+	when[2] = "thursdays";
+	var today = new Date();
+	when[3] = today; //for setting up a regular transaction from today
+	when[4] = today.getDate();
+	when[5] = today.getMonth()+1;
+	when[6] = today.getFullYear();
+	
+	return when;
+}
+
 function goEdit() {
   document.location = AppStore.App.checkQueryString("./edit.html");
 }
 
 function goCancel(){
   document.location = AppStore.App.checkQueryString("./cancel.html");
+}
+
+function goConfirm(){
+	document.location = AppStore.App.checkQueryString("./confirm.php");
 }
 
 function goHome(){
